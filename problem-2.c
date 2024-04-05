efb03961abb8098c95f3ce945ee563f2b9d9decc
OmarFaruq947 <omarfaruq.bplus.6600@gmail.com>
2024-04-05 08:24:54 +0600

    some code add

--- problem-2.c
+++ problem-2.c
@@ -1,23 +1,26 @@
-// Calculate-the-Percentage of 5 Subjects
-// It is simple to calculate the percentage of five subjects,
-// all you need to do is add all of the marks and multiply that total by 100.
-// Then divide that by the total number of marks a student is permitted to receive.
-// You will ultimately receive the results as a percentage of the 5 subjects.
-//Input: 50 20 30 40 80
+// Odd Even Program
+// A number is taken into consideration even though it may be split by two equally.
+//The remainder that is not precisely divisible by 2 is known as an odd number.
+//Simply expressed, odd numbers adopt the form n = 2k+1, whereas even numbers take the form n = 2k. Each integer will be made up of either even or odd integers.
 
+// Input:
+
+// 10
 // Output:
-// Enter marks of five subjects:
-// Total marks = 220.00
-// Average marks = 44.00
-// Percentage = 44.00
+
+// Even
+// Input:
+
+// 15
+// Output:
+
+// Odd
 
 #include <stdio.h>
 int main(){
     int num1;
     printf("Input the one number \n");
-     
       scanf("%d", &num1);
-     
 if(num1 % 2 == 0){
     printf("%d = EVEN \n", num1);
 }
